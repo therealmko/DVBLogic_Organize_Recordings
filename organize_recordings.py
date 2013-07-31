ee5ee7ff97770a6c7fe32608efff56cf1c80e593
therealmko <dkhc25@gmail.com>
2013-07-31 21:16:47 +0200

    Added chowning of downloaded files option

--- organize_recordings.py
+++ organize_recordings.py
@@ -4,11 +4,14 @@
 # Initial version by King on 26-07-2013					#
 # v1.0	King	26-07-2013						#
 # v1.1  King	29-07-2013	Add file permission settings		#
+# v1.2	King	30-07-2013	Add chowning file option		#
 #									#
 #########################################################################
 
-from os import listdir, chmod
+from pwd import getpwnam
+from os import listdir, chmod, chown
 from os.path import isfile, join
+from grp import getgrnam
 import shutil
 
 myCompareFile="organize_recordings.ini"
@@ -21,11 +24,14 @@ for line in lines:
 	vals = line.split(',')
 
 	myFiles = [ myFile for myFile in listdir(myPath) if isfile(join(myPath,myFile)) ]
-	myRecFiles = [ myRecFile for myRecFile in myFiles if myRecFile.find(vals[1].rstrip()) != -1 ]
+	myRecFiles = [ myRecFile for myRecFile in myFiles if myRecFile.find(vals[2].rstrip()) != -1 ]
 	
 	for myPersonalRec in myRecFiles:
-		shutil.move(myPersonalRec, vals[0])
-		chmod(vals[0] + "/" + myPersonalRec, 0777)
+		uid = getpwnam(vals[0]).pw_uid
+		gid = getgrnam("users").gr_gid
+		shutil.move(myPersonalRec, vals[1])
+		chmod(vals[1] + "/" + myPersonalRec, 0777)
+		chown(vals[1] + "/" + myPersonalRec, uid, gid)
 		listdir(myPath)
 
 f.close()
